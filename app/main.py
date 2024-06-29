aa9b0fc08ee0bdf9761743ec44ba111b3f8cf2fe
Roman Potykevych <rutkoroman@gmail.com>
2024-06-29 14:31:36 +0300

    add second function

--- app/main.py
+++ app/main.py
@@ -9,9 +9,12 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    
+    return {
+        "errors":[format_linter_error(error)  for error in errors],
+        "path": file_path,
+        "status": "passed" if "errors" == [] else "failed",
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return []
