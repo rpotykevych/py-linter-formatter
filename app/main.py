80879619a6818b5373fd05bc3bfc23f3f67881ce
Roman Potykevych <rutkoroman@gmail.com>
2024-06-29 14:52:27 +0300

    add third function

--- app/main.py
+++ app/main.py
@@ -17,4 +17,4 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return []
+    return [format_single_linter_file(key, value) for key, value in linter_report.items()]
