e9967bcc2774a709c75df5a98f99ddbc935966bc
Roman Potykevych <rutkoroman@gmail.com>
2024-06-29 15:40:19 +0300

    change status checking

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors":[format_linter_error(error)  for error in errors],
         "path": file_path,
-        "status": "passed" if "errors" == [] else "failed",
+        "status": "failed" if errors != [] else "passed",
     }
 
 
